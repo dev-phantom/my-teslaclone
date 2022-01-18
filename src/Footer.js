d7505836304328d1758573b9fd26aea22e1da2af
favourite1975 <godsfavour1975@gmail.com>
2022-01-18 20:08:03 +0000

    first push

--- src/Footer.js
+++ src/Footer.js
@@ -4,26 +4,11 @@ import { Link } from 'react-router-dom';
 
 const Footer = () => {
     return ( 
-        <nav>
+        <footer>
             <div className="main-flex">
-                <div className="first-flex">
-                    <span>tesla</span>
-                </div>
-                <div className="items-flex">
-                    <Link to="#!">Model S</Link>
-                    <Link to="#!">Model 3</Link>
-                    <Link to="#!">Model X</Link>
-                    <Link to="#!">Model Y</Link>
-                    <Link to="#!">Solar Roofs</Link>
-                    <Link to="#!">Solar Panels</Link>
-                </div>
-                <div className="others-flex">
-                    <Link to="#!">Shop</Link>
-                    <Link to="#!">Account</Link>
-                    <Link to="#!">Menu</Link>
-                </div>
+                
             </div>
-        </nav>
+        </footer>
      );
 }
  
