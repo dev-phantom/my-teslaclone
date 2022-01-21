37730c205ff90eb432554416d3d6b46a8f01bd3e
favourite1975 <godsfavour1975@gmail.com>
2022-01-21 04:42:09 +0000

    completed and ready for testing

--- src/Footer.js
+++ src/Footer.js
@@ -6,10 +6,10 @@ const Footer = () => {
     return ( 
         <footer>
             <div className="footer-flex">
-                <div className="inner-footer">
+
                     <div>
                         <Link to="#!">Tesla &copy; 2022</Link>
-                        <Link to="#!">privacy . legal</Link>
+                        <Link to="#!">privacy & legal</Link>
                         <Link to="#!">contact</Link>
                         <Link to="#!">careers</Link>
                     </div>
@@ -18,7 +18,6 @@ const Footer = () => {
                         <Link to="#!">engage</Link>
                         <Link to="#!">locations</Link>
                     </div>
-                </div>
 
             </div>
         </footer>
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { FaBars } from "react-icons/fa";
+import { useState } from "react";
 
 const Navbar = () => {
+    const [isOpen, setIsOpen] = useState(false);
+    const handleClick = () => setIsOpen(!isOpen);
+
     return ( 
         <nav>
             <div className="main-flex">
@@ -20,6 +25,22 @@ const Navbar = () => {
                     <Link to="#!">Shop</Link>
                     <Link to="#!">Account</Link>
                     <Link to="#!">Menu</Link>
+                </div>
+                {isOpen && <div className="mobile-flex">
+                <button onClick={handleClick} ><FaBars /></button> 
+                    <Link to="#!">Model S</Link>
+                    <Link to="#!">Model 3</Link>
+                    <Link to="#!">Model X</Link>
+                    <Link to="#!">Model Y</Link>
+                    <Link to="#!">Solar Roofs</Link>
+                    <Link to="#!">Solar Panels</Link>
+                    <Link to="#!">Shop</Link>
+                    <Link to="#!">Account</Link>
+                    <Link to="#!">Menu</Link>
+                </div>
+                }
+                <div className="menu-btn">
+                    <button onClick={handleClick} ><FaBars /></button> 
                 </div>
             </div>
         </nav>
